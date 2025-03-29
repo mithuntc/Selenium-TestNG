d2e41bf696efd00ea8073aed9556d0bc5f89e9ba
mithuntc <mithuntc783@gmail.com>
2025-03-29 15:19:30 +0530

    basic framework code completed

--- SeleniumFrameworkDesign/src/main/java/rahulshettyacademy/TestComponents/BaseTest.java
+++ SeleniumFrameworkDesign/src/main/java/rahulshettyacademy/TestComponents/BaseTest.java
@@ -1,14 +1,22 @@
 package rahulshettyacademy.TestComponents;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Properties;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import rahulshettyacademy.pageobjects.LandingPage;
@@ -31,6 +39,18 @@ public class BaseTest {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().window().maximize();
 		return driver;
+	}
+	
+	public List<HashMap<String, String>> getJsonData(String filepath) throws IOException {
+		// read json to string
+		String jsonContent =  FileUtils.readFileToString(new File(filepath),StandardCharsets.UTF_8);
+		// string to hashmap Jakson databind
+		ObjectMapper mapper = new ObjectMapper();
+		List<HashMap<String, String>> data  = 
+				mapper.readValue(jsonContent, new TypeReference<List<HashMap<String, String>>>() {});
+		
+		return data;
+
 	}
 	
 	@BeforeMethod(alwaysRun = true)
